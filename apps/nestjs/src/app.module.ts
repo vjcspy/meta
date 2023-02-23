f502e132724a5860d569f44865f2a32d06305a86
wsl <dinhkhoi.le05@gmail.com>
2023-02-23 11:04:07 +0700

    chore

--- apps/nestjs/src/app.module.ts
+++ apps/nestjs/src/app.module.ts
@@ -1,6 +1,5 @@
 import type { OnModuleInit } from '@nestjs/common';
 import { Module } from '@nestjs/common';
-import { format } from '@web/base';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,7 +10,5 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule implements OnModuleInit {
-  onModuleInit(): any {
-    console.log(format.important('Hello'));
-  }
+  onModuleInit(): any {}
 }
