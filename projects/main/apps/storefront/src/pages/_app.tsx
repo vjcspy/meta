871042bcf078b5242b44a714c82b25a03c4555e6
wsl <dinhkhoi.le05@gmail.com>
2023-03-15 23:23:20 +0700

    chore

--- projects/main/apps/storefront/src/pages/_app.tsx
+++ projects/main/apps/storefront/src/pages/_app.tsx
@@ -6,9 +6,7 @@ import {
   injectNextjsRouter,
 } from '@main/packages-web-storefront/src';
 import { resetLogRender } from '@web/base/dist/lib/logger/resetLogRender';
-import { isDevelopment } from 'chitility/dist/util/environment';
 import type { AppProps } from 'next/app';
-import getConfig from 'next/config';
 import Head from 'next/head';
 import Router, { useRouter } from 'next/router';
 import React from 'react';
@@ -17,7 +15,6 @@ import { useMemo } from 'react';
 // Boostrap Application
 bootstrap();
 
-const { publicRuntimeConfig } = getConfig();
 export default function StorefrontApp({ Component, pageProps }: AppProps) {
   resetLogRender();
   const router = useRouter();
@@ -26,35 +23,27 @@ export default function StorefrontApp({ Component, pageProps }: AppProps) {
     injectNextjsRouter(router, Router);
   }, [router]);
 
-  const AppPage = useMemo(() => {
-    return (
-      <>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-          <meta
-            name="description"
-            content="Storefront Demo site Storefront Demo site"
-          />
-          <link
-            rel="shortcut icon"
-            type="image/png"
-            sizes="32x32"
-            href="/icons8_fav_32×32.png"
-          />
-          <title>Storefront Demo site</title>
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
-  }, [pageProps]);
-
-  if (publicRuntimeConfig?.nextVersion == 12 && isDevelopment()) {
-    return <React.StrictMode>{AppPage}</React.StrictMode>;
-  } else {
-    return <>{AppPage}</>;
-  }
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta
+          name="description"
+          content="Storefront Demo site Storefront Demo site"
+        />
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons8_fav_32×32.png"
+        />
+        <title>Storefront Demo site</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
