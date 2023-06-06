7dac77b4b196fff9b5f722094f75d3d260bd4b75
wsl <mr.vjcspy@gmail.com>
2023-06-06 09:57:57 +0700

    feat: creat job

--- projects/stock/apps/api/src/modules/stock-info/values/sync.values.ts
+++ projects/stock/apps/api/src/modules/stock-info/values/sync.values.ts
@@ -6,4 +6,5 @@ export class SyncValues {
   static EXCHANGE_KEY = 'stock.info.sync';
 
   static ORDER_MATCHING_KEY = 'stock.info.sync.order-matching';
+  static JOB_SYNC_OM_KEY = 'JOB_SYNC_OM_KEY';
 }
