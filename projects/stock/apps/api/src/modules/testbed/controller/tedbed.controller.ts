f885cef0528d74400f3538c36802bb225ed22af8
wsl <mr.vjcspy@gmail.com>
2023-09-09 15:24:20 +0700

    chore

--- projects/stock/apps/api/src/modules/testbed/controller/tedbed.controller.ts
+++ projects/stock/apps/api/src/modules/testbed/controller/tedbed.controller.ts
@@ -45,9 +45,4 @@ export class TedbedController {
       .getConnection()
       .publish(TESTBED_EXCHANGE_KEY, TESTBED_ROUTING_KEY, Math.random());
   }
-
-  @Get('test-clear-chat')
-  testClearChat() {
-    return this.testBedService.clearChat();
-  }
 }
