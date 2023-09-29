8d7bb0e67a55a067e7664e8edf5e050f6b4772b1
wsl <mr.vjcspy@gmail.com>
2023-09-29 20:43:22 +0700

    chore

--- projects/stock/apps/web/pages/_document.tsx
+++ projects/stock/apps/web/pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap"
+                />
             </Head>
             <body>
                 <Main />
