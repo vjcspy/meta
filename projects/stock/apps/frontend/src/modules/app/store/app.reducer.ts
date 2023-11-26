38c6a4ef57d47bdf6b8cc72eb8b3ad179729fb59
vjcspy <mr.vjcspy@gmail.com>
2023-11-26 10:28:19 +0700

    chore: refactor

--- projects/stock/apps/frontend/src/modules/app/store/app.reducer.ts
+++ projects/stock/apps/frontend/src/modules/app/store/app.reducer.ts
@@ -1,4 +1,4 @@
-import type { ApiResponseDto } from '@src/modules/app/dto/api-response.dto';
+import type { ApiResponse } from '@modules/app/type/api-response';
 import { appInitialState } from '@src/modules/app/store/app.state';
 import type { PayloadAction } from '@stock/packages-redux';
 import { createSlice } from '@stock/packages-redux';
@@ -7,7 +7,7 @@ export const appSlice = createSlice({
   name: 'app',
   initialState: appInitialState,
   reducers: {
-    fetchApiError: (state, action: PayloadAction<ApiResponseDto>) => {
+    fetchApiError: (state, action: PayloadAction<ApiResponse>) => {
       console.error(`fetchApiError path: '${action?.payload?.path}'`);
       return state;
     },
