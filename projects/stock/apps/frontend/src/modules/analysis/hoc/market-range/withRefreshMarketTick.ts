21da01b16fcfcbee572e37476723fe3759a85bd9
wsl <mr.vjcspy@gmail.com>
2023-12-19 22:26:51 +0700

    chore

--- projects/stock/apps/frontend/src/modules/analysis/hoc/market-range/withRefreshMarketTick.ts
+++ projects/stock/apps/frontend/src/modules/analysis/hoc/market-range/withRefreshMarketTick.ts
@@ -1,9 +1,6 @@
 import { refreshTickRequest$ } from '@modules/analysis/hoc/withRefreshTick';
-import { ANALYSIS_ACTIONS } from '@modules/analysis/store/analysis.actions';
-import { MarketTicks } from '@modules/analysis/util/ticks/market-ticks';
 import { CommonValue } from '@modules/analysis/value/common.value';
 import { useSelectFromState } from '@src/store/selectFromState';
-import { useAppDispatch } from '@src/store/useAppDispatch';
 import { isTradingTime } from '@stock/packages-com/dist/util/isTradingTime';
 import { createUiHOC } from '@web/ui-extension/dist';
 import { forEach } from 'lodash-es';
