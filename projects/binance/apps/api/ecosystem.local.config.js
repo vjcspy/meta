0e3c8c1f35555b8fa1c23684636ad7d00d1164b6
vjcspy <mr.vjcspy@gmail.com>
2024-03-04 22:48:57 +0700

    chore

--- projects/binance/apps/api/ecosystem.local.config.js
+++ projects/binance/apps/api/ecosystem.local.config.js
@@ -3,11 +3,11 @@ module.exports = {
     {
       name: 'mc_dev',
       script: './dist/main.js',
-      instances: '6',
+      instances: '2',
       instance_var: 'INSTANCE_ID',
       exec_mode: 'cluster',
       env: {
-        APP_NAME: 'ms_dev',
+        APP_NAME: 'mc_dev',
         CRON_ENABLE: false,
         QUEUE_CONSUMER_ENABLE: true,
       },
