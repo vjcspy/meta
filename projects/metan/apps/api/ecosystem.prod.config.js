e2faac5c6d00b9df84880b809f4caf520f7fd760
wsl <mr.vjcspy@gmail.com>
2024-05-13 16:12:19 +0700

    chore

--- projects/metan/apps/api/ecosystem.prod.config.js
+++ projects/metan/apps/api/ecosystem.prod.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'mc_rq',
+      name: 'metan_mexc_rq',
       script: './dist/main.js',
       instances: '2',
       instance_var: 'INSTANCE_ID',
       exec_mode: 'cluster',
       env: {
-        APP_NAME: 'mc_rq',
+        APP_NAME: 'metan_mexc',
         NODE_ENV: 'production',
         CRON_ENABLE: false,
         QUEUE_CONSUMER_ENABLE: false,
