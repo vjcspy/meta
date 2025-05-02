fd7487dd3bd81bdae96bfe5bb2951bc743f45394
win <mr.vjcspy@gmail.com>
2025-05-02 10:12:34 +0700

    feat: map maker api already check current existing room before create a new one

--- projects/unity/apps/server/src/modules/map-maker/controllers/enter.ts
+++ projects/unity/apps/server/src/modules/map-maker/controllers/enter.ts
@@ -3,25 +3,31 @@ import { extractToken } from '@modules/auth/util';
 import type { MapOptions } from '@modules/declaration/map';
 import { matchMaker } from 'colyseus';
 import type { Request, Response } from 'express';
+import _ from 'lodash';
 
 import { MapIndex } from '../../../rooms/maps';
 
 export const enter = async (req: Request, res: Response) => {
-  const { mapId } = req.query;
+  const { mapId, forceNewInstance } = req.query;
   const useData: UserData = (req as any).auth;
 
   // TODO: Create new api to get current existing map
-  // if (!forceNewInstance) {
-  //   // Check xem đang có instance nào của map đó không
-  //   const existingRoom = await matchMaker.query({
-  //     name: MapIndex.MAP_V1,
-  //   });
-  //   console.log('existingRoom', existingRoom);
-  //   const existingMap = _.find(
-  //     existingRoom,
-  //     (room) => room.metadata.creatorId === useData.id,
-  //   );
-  // }
+  if (!forceNewInstance) {
+    // Check xem đang có instance nào của map đó không
+    const existingRoom = await matchMaker.query({
+      name: MapIndex.MAP_V1,
+    });
+    console.log('existingRoom', existingRoom);
+    const existingMap = _.find(
+      existingRoom,
+      (room) => room.metadata.creatorId === useData.id,
+    );
+
+    if (existingMap) {
+      const reservation = await matchMaker.joinById(existingMap.roomId, {});
+      return res.json(reservation);
+    }
+  }
 
   const reservation = await matchMaker.create(
     MapIndex.MAP_V1,
