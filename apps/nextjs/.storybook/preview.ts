07c8863b63c413a7eafdb39ffd6730a917063cdb
win <mr.vjcspy@gmail.com>
2025-08-02 18:56:19 +0700

    chore

--- apps/nextjs/.storybook/preview.ts
+++ apps/nextjs/.storybook/preview.ts
@@ -1,14 +1,20 @@
-import type { Preview } from '@storybook/nextjs'
-import '../src/styles/globals.css'
+import "../src/styles/globals.css";
+
+import type { Preview } from "@storybook/nextjs";
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
+    docs: {
+      toc: true,
+    },
+    layout: "centered",
   },
 };
 
